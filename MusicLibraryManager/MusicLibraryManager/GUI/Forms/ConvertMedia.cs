3de24ef319235dbdd35183e22d2db92cabe9a282
Luca Pozzi <lpozzi44@hotmail.it>
2016-01-23 03:00:32 +0100

    Aggiunto Form per la conversione dei file audio Agginta classe Playlist Aggiunta DLL per il Json

---
+++ MusicLibraryManager/MusicLibraryManager/GUI/Forms/ConvertMedia.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MusicLibraryManager.GUI.Forms
+{
+    public partial class ConvertMedia : Form
+    {
+        public ConvertMedia()
+        {
+            InitializeComponent();
+        }
+
+        private void ConvertMedia_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ MusicLibraryManager/MusicLibraryManager/Playlist.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicLibraryManager
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class Playlist
+    {
+        [JsonProperty]
+        String Name;
+        [JsonProperty]
+        public MyFileSystemPlus FileSystem;
+
+        [JsonIgnore]
+        public String Path;
+
+        public Playlist()
+        {
+
+        }
+        public Playlist(String Path)
+        {
+            this.Path = Path;
+        }
+        public Playlist(String Path,String Nome)
+        {
+            Name = Nome;
+            this.Path = Path;
+        }
+
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
